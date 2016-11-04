e57c73276bcec8772d02e60b2beec994fdea6fd4
Lawrence Law <d201.c317@gmail.com>
2016-11-04 12:29:32 +0000

    Ruby Coding Styles Update

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,23 +1,22 @@
 #!/usr/bin/env ruby
 
 require "bunny"
+require "json"
 
 connection = Bunny.new
 connection.start
 
 channel    = connection.create_channel
-queueIn    = channel.queue("in")
-queueOut   = channel.queue("out")
+queue_in   = channel.queue("in")
+queue_out  = channel.queue("out")
 prng       = Random.new
 sysid      = ENV["SYSID"] || prng.rand(100)
 
-puts " [!] Waiting for messages in #{queueIn.name}. To exit press CTRL+C"
+puts " [!] Waiting for messages in #{queue_in.name}. To exit press CTRL+C"
 
-queueIn.subscribe(:block => true) do |delivery_info, properties, body|
+queue_in.subscribe(block: true) do |_, properties, body|
   puts " [I] Received #{body} @ #{properties.correlation_id}"
-
-  msg = "{\"message\": \"#{body}\", \"taskID\": \"#{properties.correlation_id}\", \"sysid\": \"#{sysid}\"}"
-  channel.default_exchange.publish(msg, :routing_key => queueOut.name, :correlation_id => properties.correlation_id)
+  msg = { message: body, taskid: properties.correlation_id, sysid: sysid }
+  channel.default_exchange.publish(JSON.generate(msg), routing_key: queue_out.name, correlation_id: properties.correlation_id)
   puts " [O] Sent #{msg}"
-
 end
